d27f7d5aff59a7f380c53706294b9069ce3ebe32
Asm-Def <willywu@foxmail.com>
2022-05-14 22:13:12 +0800

    feat:输出.d.ts; feat:测试resize

--- examples/index.js
+++ examples/index.js
@@ -1,12 +1,40 @@
 // import SignPad from "/dist/signpad.js";
 let canvasList = document.querySelectorAll("canvas.signpad");
+function $(selector) {
+    return document.querySelector(selector);
+}
 
 let config = {
-    bgColor: "#eee"
+    maxWidth: 15,
+    bgColor: "#fff"
 };
-console.log(SignPad)
 let signpadList = Array(canvasList.length).fill(null);
+function resize(ev) {
+    let rat_wh = 2;
+    for(var i = 0;i < canvasList.length;i++) {
+        let div = canvasList[i].parentElement;
+        let tmp = canvasList[i];
+        let rect = div.getBoundingClientRect();
+        let w = rect.width, h = rect.height;
+        let t = Math.min(w-10, (h-10)*rat_wh);
+        w = Math.floor(t), h = Math.floor(t / rat_wh);
+        console.log(w, h);
+        signpadList[i].width = w;
+        signpadList[i].height = h;
+        tmp.style.width  = w + "px";
+        tmp.style.height = h + "px";
+        tmp.width = w;
+        tmp.height = h;
+    }
+}
 for(var i = 0; i < canvasList.length; i++){
     signpadList[i] = new SignPad.SmoothSignature(canvasList[i], config);
     console.log(i, signpadList[i]);
 }
+resize();
+window.addEventListener("resize", resize);
+$("#clear").addEventListener("click", () => {
+    for(var i = 0; i < signpadList.length; i++){
+        signpadList[i].clear();
+    }
+})
